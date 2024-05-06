43cfe99c3f881d45669797f891f6e5c5524b57cc
fe-xiaoxin <zxin088@gmail.com>
2024-05-06 16:51:12 +0800

    fix: 修复路由重定向

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: "/home", redirect: "/markdown" },
+  { path: "/", redirect: "/markdown" },
   { path: "/markdown", component: () => import("../pages/Markdown.vue") },
   { path: "/authorize", component: () => import("../pages/Authorize.vue") },
 ];
