0852f12e75d6c1ada89db4bda86c3bcc129c09f4
fe-xiaoxin <zxin088@gmail.com>
2024-05-06 16:22:25 +0800

    test: 验证

---
+++ src/pages/Authorize.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const code = route.query.code;
+const client_id =
+  "250ae102f9d11277e9c0268104e4dc5b7b75c3f697033e88f4c54417d6ddd01c";
+const client_secret =
+  "3192a206423155e0f779592255c69b1c747283204c3aa7bcfdd44ab6bf85c572";
+const redirect_uri = "https://online-toolbox.vercel.app/authorize";
+
+axios
+  .post("https://gitee.com/oauth/token", {
+    grant_type: "authorization_code",
+    code,
+    client_id,
+    client_secret,
+    redirect_uri,
+  })
+  .then((res) => {
+    console.log(res.data);
+  });
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
   { path: "/", component: () => import("../pages/Markdown.vue") },
   { path: "/markdown", component: () => import("../pages/Markdown.vue") },
+  { path: "/authorize", component: () => import("../pages/Authorize.vue") },
 ];
 
 export const router = createRouter({
